c194d090d03703cba0e5b068272e2882ee346c0f
nelsontky <nelson@ccb.wtf>
2020-07-01 15:56:29 +0800

    Updates

---
+++ precache-manifest.6519d6131ecbdb086238f468b0a84f07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c1add1fe2f02ec22228f7854b7a2a33",
+    "url": "/I-AM-A-PAP-IB/index.html"
+  },
+  {
+    "revision": "a402613750bfc159c881",
+    "url": "/I-AM-A-PAP-IB/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "ff793e94a529bc5b19d3",
+    "url": "/I-AM-A-PAP-IB/static/js/2.aeebf790.chunk.js"
+  },
+  {
+    "revision": "07707997fc472e36c0620ae7288d84e4",
+    "url": "/I-AM-A-PAP-IB/static/js/2.aeebf790.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a402613750bfc159c881",
+    "url": "/I-AM-A-PAP-IB/static/js/main.6b3993fb.chunk.js"
+  },
+  {
+    "revision": "adcfbc19eaa7a58f36e5",
+    "url": "/I-AM-A-PAP-IB/static/js/runtime-main.5c9c83dd.js"
+  }
+]);
